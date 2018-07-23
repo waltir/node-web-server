cc506b97396c6268e581f98d074bdf2ea0ea83d5
Walter Lyons <walterlyons@walters-mini.home>
2018-07-23 07:12:24 -0700

    Setup Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+
+const port = process.env.PORT || 3000;
 var app = express();
 
 // SETTING UP EXPRESS STATIC DIRECTORY
@@ -63,6 +65,7 @@ app.get('/bad', (req, res) => {
   res.send(errorMessage.error);
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
