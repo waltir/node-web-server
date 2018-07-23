e0a70bcabd53eb24a88848cdae717fc07a070260
Walter Lyons <walterlyons@walters-mini.home>
2018-07-23 07:21:27 -0700

    Adding Projects page

--- server.js
+++ server.js
@@ -57,6 +57,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Welcome to my portfolio!'
+  });
+});
+
 var errorMessage = {
   error: 'Page not found'
 }
